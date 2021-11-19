58a27e90d09a24d275f932981a703a722ccc8723
Murad_777 <murad.gadzhiev1999@mail.ru>
2021-11-19 18:29:43 +0300

    v0.1

---
+++ Auth_Users/Model/DataUsers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth_Users.Model
+{
+   public class DataUsers
+   {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DataUsers(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+   }
+
+}
---
+++ Auth_Users/Program.cs
@@ -0,0 +1,17 @@
+﻿using Auth_Users.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Auth_Users
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
